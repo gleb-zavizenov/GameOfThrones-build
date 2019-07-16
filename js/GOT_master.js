d1c3d9de252e77caf0c3cbef3b2da0a35fc0f6d6
Zavizenov <g_zavizenov@tss-hr420-dm07.fanshawec.ca>
2019-07-16 18:04:23 -0400

    Finished js slider

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -6,67 +6,77 @@
   const lightbox = document.querySelector(".lightbox");
   const closeLightboxBtn = document.querySelector(".close-lightbox");
   const banners = document.querySelectorAll(".banner");
-  const houseVideo = document.querySelector(".house-video");
+  const houseVideo = document.querySelector(".house-video"),
+        bannerImages = document.querySelector("#houseImages");
 
   // Function that fires every time sigil is pressed
   function popLightBox(e){
     // debug this so far and make sure the event handeling works
     //debugger;
-    if(e.target.classList.contains("stark")){
-      console.log("starks");
-      //show starks img
-      for(let i = 0; i < banners.length; i++){
-        banners[i].classList.remove("active");
-      }
-      banners[0].classList.add("active");
-    }
-    if(e.target.classList.contains("baratheon")){
-      console.log("baratheon");
-      //show baratheon img
-      for(let i = 0; i < banners.length; i++){
-        banners[i].classList.remove("active");
-      }
-      banners[1].classList.add("active");
-    }
-    if(e.target.classList.contains("lannister")){
-      console.log("lannister");
-      //show lannister img
-      for(let i = 0; i < banners.length; i++){
-        banners[i].classList.remove("active");
-      }
-      banners[2].classList.add("active");
-    }
-    if(e.target.classList.contains("tully")){
-      console.log("tully");
-      //show tully img
-      for(let i = 0; i < banners.length; i++){
-        banners[i].classList.remove("active");
-      }
-      banners[3].classList.add("active");
-    }
-    if(e.target.classList.contains("greyjoy")){
-      console.log("greyjoy");
-      //show greyjoy img
-      for(let i = 0; i < banners.length; i++){
-        banners[i].classList.remove("active");
-      }
-      banners[4].classList.add("active");
-    }
-    if(e.target.classList.contains("arryn")){
-      console.log("arryn");
-      //show arryn img
-      for(let i = 0; i < banners.length; i++){
-        banners[i].classList.remove("active");
-      }
-      banners[5].classList.add("active");
-    }
+    // if(e.target.classList.contains("stark")){
+    //   console.log("starks");
+    //   //show starks img
+    //   for(let i = 0; i < banners.length; i++){
+    //     banners[i].classList.remove("active");
+    //   }
+    //   banners[0].classList.add("active");
+    // }
+    // if(e.target.classList.contains("baratheon")){
+    //   console.log("baratheon");
+    //   //show baratheon img
+    //   for(let i = 0; i < banners.length; i++){
+    //     banners[i].classList.remove("active");
+    //   }
+    //   banners[1].classList.add("active");
+    // }
+    // if(e.target.classList.contains("lannister")){
+    //   console.log("lannister");
+    //   //show lannister img
+    //   for(let i = 0; i < banners.length; i++){
+    //     banners[i].classList.remove("active");
+    //   }
+    //   banners[2].classList.add("active");
+    // }
+    // if(e.target.classList.contains("tully")){
+    //   console.log("tully");
+    //   //show tully img
+    //   for(let i = 0; i < banners.length; i++){
+    //     banners[i].classList.remove("active");
+    //   }
+    //   banners[3].classList.add("active");
+    // }
+    // if(e.target.classList.contains("greyjoy")){
+    //   console.log("greyjoy");
+    //   //show greyjoy img
+    //   for(let i = 0; i < banners.length; i++){
+    //     banners[i].classList.remove("active");
+    //   }
+    //   banners[4].classList.add("active");
+    // }
+    // if(e.target.classList.contains("arryn")){
+    //   console.log("arryn");
+    //   //show arryn img
+    //   for(let i = 0; i < banners.length; i++){
+    //     banners[i].classList.remove("active");
+    //   }
+    //   banners[5].classList.add("active");
+    // }
     lightbox.classList.add("show-lightbox");
     houseVideo.play();
   }
 
+  function animateBanners(){
+    //we need to get an offset that we can multiply
+    let offset = 600,
+        multiplier = this.dataset.offset;
+
+    houseImages.style.right = `${offset * multiplier}px`;
+
+  }
+
   // Adding event listener for each sigil
   sigils.forEach(sigil => {
-    sigil.addEventListener("click", popLightBox);
+    sigil.addEventListener("click", animateBanners);
   })
 
   closeLightboxBtn.addEventListener("click", (e) =>{
